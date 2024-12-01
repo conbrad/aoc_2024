e712f1b77842666c83e7b18e5797eb5267aa668e
Conor Brady <con.brad@gmail.com>
2024-12-01 09:53:13 -0800

    Finish day 1

--- aoc_2024/day1.py
+++ aoc_2024/day1.py
@@ -1,10 +1,25 @@
 import os
-def solve(list1, list2):
+def compute_distance(list1, list2):
     assert len(list1) == len(list2)
     total_distance = 0
     for x, y in zip(list1, list2):
         total_distance += abs(x - y)
     return total_distance
+
+def compute_similarity(list1, list2):
+    assert len(list1) == len(list2)
+    location_counts = {}
+    for y in list2:
+        curr_count = location_counts.get(y, 0)
+        location_counts[y] = curr_count + 1
+    
+    similarity_score = 0
+    for x in list1:
+        similarity_score += x * location_counts.get(x, 0)
+
+    return similarity_score
+
+
 
 if __name__ == "__main__":
     list1 = []
@@ -18,4 +33,6 @@ if __name__ == "__main__":
 
     list1.sort()
     list2.sort()
-    print(solve(list1, list2))
+    print(compute_distance(list1, list2))
+    print(compute_similarity(list1, list2))
+
